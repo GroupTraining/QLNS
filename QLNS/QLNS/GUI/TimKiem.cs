50a919a5715c7c8b5ca0e5c488b95af28234f6e8
kieutomm <36162705+kieutomm@users.noreply.github.com>
2018-03-11 22:56:17 +0700

    Tìm Kiếm 1.2

--- QLNS/QLNS/GUI/TimKiem.cs
+++ QLNS/QLNS/GUI/TimKiem.cs
@@ -13,24 +13,40 @@ namespace QLNS.GUI
 {
     public partial class TimKiem : Form
     {
+        Bus bus = new Bus();
         public TimKiem()
         {
             InitializeComponent();
+
+            dataGridView1.DataSource = bus.get_nhanvien();
         }
 
         private void btnCapnhat_Click_1(object sender, EventArgs e)
         {
-            
+            dataGridView1.DataSource = bus.get_nhanvien();
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            txtManv.Text = dataGridView1.CurrentRow.Cells["MaNV"].Value.ToString();
+            txtHoten.Text = dataGridView1.CurrentRow.Cells["Hoten"].Value.ToString();
+            txtNs.Text = dataGridView1.CurrentRow.Cells["NS"].Value.ToString();
+            txtPb.Text = dataGridView1.CurrentRow.Cells["TenPB"].Value.ToString();
+            if (dataGridView1.CurrentRow.Cells["GT"].Value.ToString() == "False")
+            {
+                txtGt.Text = "Nam";
+            }
+            else
+            {
+                txtGt.Text = "Nữ";
+            }
+
+            txtDa.Text = dataGridView1.CurrentRow.Cells["TenDA"].Value.ToString();
         }
 
         private void btnTimkiem_Click_1(object sender, EventArgs e)
         {
-            
+            dataGridView1.DataSource = bus.get_nhanvien(txtGiatri.Text);
         }
     }
 }
