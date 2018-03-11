fdae5cadc2d763de8559b8ba7d8126b70b708a59
hungzr <36071597+hungzr@users.noreply.github.com>
2018-03-11 22:07:41 +0700

    code sửa + tìm kiếm để sửa

--- QLNS/BUS/Bus.cs
+++ QLNS/BUS/Bus.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL;
+using System.Globalization;
 
 namespace BUS
 {
@@ -45,6 +46,32 @@ namespace BUS
 
             return database;
         }
+        public object Search(string text)
+        {
+            var database = from u in data.NhanViens
+                           from t in data.PhongBans
+                           from v in data.PhanCongs
+                           from z in data.DuAns
+                           where u.MaNV == v.MaNV
+                           where v.MaDA == z.MaDA
+                           where u.MaPB == t.MaPB
+                           select new
+                           {
+                               MaNV = u.MaNV,
+                               HoTen = u.HoTen,
+                               NS = u.NS,
+                               GT = u.GT,
+                               MaPB = t.MaPB,
+                               MaDA = v.MaDA
+                           } into timkiemNV
+                           where timkiemNV.MaNV.Contains(text)
+                               || timkiemNV.HoTen.Contains(text)
+                               || timkiemNV.MaPB.Contains(text)
+                               || timkiemNV.MaDA.Contains(text)
+                           select timkiemNV;
+            return database;
+
+        }
         public object Delete(string ma)
         {
             var nv = from a in data.NhanViens
@@ -58,6 +85,26 @@ namespace BUS
             data.SubmitChanges();
             return 1;
         }
+        public object Edit(string ma, string ten, string ns, string gt, string ma_pb)
+        {
+            NhanVien nv = data.NhanViens.Single(a => a.MaNV == ma);
+            nv.MaNV = ma;
+            nv.HoTen = ten;
+            nv.NS = DateTime.ParseExact(ns, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            if (gt == "Nam")
+            {
+                nv.GT = false;
+            }
+            if (gt == "Nữ")
+            {
+                nv.GT = true;
+            }
+            nv.MaPB = ma_pb;
+
+            data.SubmitChanges();
+            return 1;
+        }
+
         public object get_nhanvien()
         {
             var nhanvien = from u in data.NhanViens
